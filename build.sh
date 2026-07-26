09abfbc6b74741c6a4b5cc33b96d1b089bb57512
Moeed-S-E <muhammadmoeed3737@outlook.com>
2026-07-26 08:50:13 +0500

    Revert to standard pip for Render environment compatibility

--- build.sh
+++ build.sh
@@ -5,9 +5,7 @@ set -o errexit
 # Ensure Tectonic is executable
 chmod +x tectonic
 
-# Install uv for blazing fast dependency resolution
-pip install uv
-uv pip install --system -r requirements.txt
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
