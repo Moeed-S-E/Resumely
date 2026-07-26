e1385fde8060d929f338a949d34c6dccb48f2b71
Moeed-S-E <muhammadmoeed3737@outlook.com>
2026-07-26 09:05:31 +0500

    Add root URL redirect to /resume-ats/

--- core/urls.py
+++ core/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/resume-ats/', permanent=False), name='index'),
     path('admin/', admin.site.urls),
     path('resume-ats/', include('resume_ats_analyzer.urls')),
 ]
