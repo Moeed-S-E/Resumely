fbfa69ea8dc092033a21fea940187bbbc4123bfc
Moeed-S-E <muhammadmoeed3737@outlook.com>
2026-07-26 08:36:52 +0500

    Update build.sh to use uv

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ set -o errexit
 # Ensure Tectonic is executable
 chmod +x tectonic
 
-pip install -r requirements.txt
+# Install uv for blazing fast dependency resolution
+pip install uv
+uv pip install --system -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
